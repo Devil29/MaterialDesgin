931c59cc26b0d675e565ce4162aa7902d7128cc2
Devil29 <vishalraj48@gmail.com>
2016-10-25 13:33:14 +0530

    Added all the three Fragments:

---
+++ app/src/main/java/com/android/materialdesign/fragment/ThreeFragment.java
@@ -0,0 +1,32 @@
+package com.android.materialdesign.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.android.materialdesign.R;
+
+/**
+ * Created by vishal.kumar1 on 25/10/16.
+ */
+
+public class ThreeFragment extends Fragment {
+    public ThreeFragment(){
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.fragment_three, container, false);g
+        return view;
+    }
+}
