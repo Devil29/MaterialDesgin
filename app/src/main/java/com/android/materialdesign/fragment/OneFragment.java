39222b43efd0cf7ff17bfebb65cbfda407b2f224
Devil29 <vishalraj48@gmail.com>
2016-10-25 11:26:18 +0530

    Added Fragment_one for tabbed activity

---
+++ app/src/main/java/com/android/materialdesign/fragment/OneFragment.java
@@ -0,0 +1,33 @@
+package com.android.materialdesign.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.android.materialdesign.R;
+
+/**
+ * Created by vishal.kumar1 on 25/10/16.
+ */
+
+public class OneFragment extends Fragment {
+
+    public OneFragment(){
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_one, container ,false);
+        return view;
+    }
+}
